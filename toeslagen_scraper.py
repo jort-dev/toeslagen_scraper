86b8172d21dbe3c0ae32a6f72797f8336e42e161
Jort <jortdeveloper@gmail.com>
2024-09-23 22:43:24 +0200

    working poc

--- toeslagen_scraper.py
+++ toeslagen_scraper.py
@@ -1,34 +1,12 @@
 import asyncio
 import re
+import tkinter as tk
 
 from playwright.async_api import async_playwright
 
-DELAY = 0.1  # seconds
+from test.test_gegevens import vul_test_gegevens_in
 
-
-async def fill_field(page, selector, value, delay=10):
-    locator = page.locator(selector)
-    await locator.scroll_into_view_if_needed()
-    await locator.type(value, delay=delay)
-
-
-async def check_checkbox(page, input_selector):
-    locator = page.locator(input_selector)
-    await locator.scroll_into_view_if_needed()
-    await locator.click()
-
-
-async def select_radio(page, label_selector):
-    locator = page.locator(label_selector)
-    await locator.scroll_into_view_if_needed()
-    await locator.click()
-
-
-async def select_dropdown(page, selector, value):
-    # locator = page.locator("label[for='V2-1_pbt_False']")
-    locator = page.locator(selector)
-    await locator.scroll_into_view_if_needed()
-    await locator.select_option(value)
+STEP = 50  # euros
 
 
 def get_result(results, tag):
@@ -54,6 +32,7 @@ async def get_results(page):
         results.append(txt)
     return results
 
+
 def write_results(huurtoeslag, zorgtoeslag, kinderopvangtoeslag, kindergevonden_budget):
     pass
 
@@ -66,45 +45,9 @@ async def main():
         page = await context.new_page()
 
         print("Website laden")
-        await page.goto(
-            'https://www.belastingdienst.nl/wps/wcm/connect/nl/toeslagen/content/hulpmiddel-proefberekening-toeslagen')
-        await asyncio.sleep(DELAY)
+        await page.goto('https://www.belastingdienst.nl/wps/wcm/connect/nl/toeslagen/content/hulpmiddel-proefberekening-toeslagen')
 
-        print("Kiezen: 2024")
-        await select_dropdown(page, "#V1-1_pbt", "2024")
-        await asyncio.sleep(DELAY)
-
-        print("Kiezen: zorgtoeslag")
-        await check_checkbox(page, "input#V1-3_pbt_4")
-        await asyncio.sleep(DELAY)
-
-        print("Kiezen: geen toeslagpartner")
-        await select_radio(page, "label[for='V2-1_pbt_False']")
-        await asyncio.sleep(DELAY)
-
-        print(f"Invoeren: geboortedatum")
-        await fill_field(page, "#V2-3_pbt-1", "1")
-        await asyncio.sleep(DELAY)
-        await fill_field(page, "#V2-3_pbt-2", "1")
-        await asyncio.sleep(DELAY)
-        await fill_field(page, "#V2-3_pbt-3", "2000", 100)
-        await asyncio.sleep(DELAY)
-
-        print(f"Invoeren: salaris 543")
-        await fill_field(page, "#V3-10_pbt", "543", 100)
-        await asyncio.sleep(DELAY)
-
-        print("Kiezen: premie")
-        await select_radio(page, "label[for='V3-12_pbt_True']")
-        await asyncio.sleep(DELAY)
-
-        print("Kiezen: laag vermogen")
-        await select_radio(page, "label[for='V11-11_pbt_False']")
-        await asyncio.sleep(DELAY)
-
-        print("Klikken: Toon resultaten")
-        locator = page.locator("#butResults_pbt")
-        await locator.click()
+        await vul_test_gegevens_in(page)  # TEST
 
         print(f"Resultaten ophalen")
         results = await get_results(page)
@@ -115,6 +58,29 @@ async def main():
         kinderopvangtoeslag = get_result(results, "kinderopvangtoeslag")
         kindergebonden_budget = get_result(results, "kindgebonden budget")
         print(f"{zorgtoeslag=}, {huurtoeslag=}, {kinderopvangtoeslag=}, {kindergebonden_budget=}")
+
+        print(f"Start berekening vanaf: {inkomen}")
+        while True:
+            inkomen += STEP
+
+            # wijzig invoer
+            await page.evaluate("window.scrollTo(0, 0)")
+            locator = page.locator("#butWijzig_pbt")
+            await locator.click()
+
+            await instant_fill_field(page, "#V3-10_pbt", str(inkomen))
+
+            await page.evaluate("window.scrollTo(0, 0)")
+            locator = page.locator("#butResults_pbt")
+            await locator.click()
+
+            results = await get_results(page)
+            zorgtoeslag = get_result(results, "zorgtoeslag")
+            huurtoeslag = get_result(results, "huurtoeslag")
+            kinderopvangtoeslag = get_result(results, "kinderopvangtoeslag")
+            kindergebonden_budget = get_result(results, "kindgebonden budget")
+            print(f"{inkomen=}: {zorgtoeslag=}, {huurtoeslag=}, {kinderopvangtoeslag=}, {kindergebonden_budget=}")
+            await asyncio.sleep(2)
 
         print(f"Klaar")
         while True:
